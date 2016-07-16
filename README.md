98eddd8958681ebe2a073e07532bb1ed8f09f8c9
Richard Davis <rdavis.bacs@gmail.com>
2016-07-16 19:42:19 -0400

    stuff and junk

---
+++ README.md
@@ -0,0 +1,3 @@
+## HelloWorld Kodi Add-on
+
+Forked from [script.hello.world](https://github.com/zag2me/script.hello.world)
--- addon.py
+++ addon.py
@@ -1,11 +1,11 @@
 import xbmcaddon
 import xbmcgui
- 
-addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
- 
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
- 
+
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+
+line1 = "Waddup."
+line2 = "This is a Scripty McScriptface."
+line3 = "Written using a type of Amazon snake."
+
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
